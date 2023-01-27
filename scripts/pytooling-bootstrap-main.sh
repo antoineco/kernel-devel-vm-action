b86408640b5edf173cbc695e32ec7179a97315b7
Antoine Cotten <hello@acotten.com>
2023-01-27 09:31:15 +0100

    chore: update Actions due to Node.js 12 deprecation

--- scripts/pytooling-bootstrap-main.sh
+++ scripts/pytooling-bootstrap-main.sh
@@ -6,6 +6,9 @@
 set -eu -o pipefail
 
 echo '::group::Bootstrap Lima instance'
+# Since v0.4.4, with-post-step does not append a newline character after POST=true.
+# https://github.com/pyTooling/Actions/commit/97fd0e59278300263d86ce71e1d45f295bf76cec
+echo >>"$GITHUB_STATE"
 echo "LIMA_INSTANCE=${LIMA_INSTANCE}" >>"$GITHUB_STATE"
 "${BASH_SOURCE[0]%/*}"/bootstrap_builder.sh "$1" "$2" "$3" "$4"
 echo '::endgroup::'
